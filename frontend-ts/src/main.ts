e08eafcd7b862191f3a00d523677f21ffdfef148
Van Salido <vansalido@gmail.com>
2023-01-22 14:29:44 +0800

    no relevant changes

--- frontend-ts/src/main.ts
+++ frontend-ts/src/main.ts
@@ -7,7 +7,12 @@ import router from "./router";
 import axios from "axios";
 import VueClickAway from "vue3-click-away";
 import VueCameraLib from "vue-camera-lib";
-import Camera from 'simple-vue-camera'
+import Camera from 'simple-vue-camera';
+
+
+
+
+
 
 const app = createApp(App);
 const baseURL = location.protocol + "//" + location.hostname + ":8000";
@@ -18,5 +23,8 @@ app.use(createPinia());
 app.use(router, axios);
 app.use(VueClickAway);
 app.use(VueCameraLib);
+
+
+
 
 app.mount("#app");
